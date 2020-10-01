11f5213778d0f5b74bf559c130935075f14e5005
coriander31415 <coriander31415@gmail.com>
2020-10-01 12:45:10 +0300

    feat: solve typical arrays problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    return (!array || array.length === 0) ? 0 : Math.min.apply(null, array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    return (!array || array.length === 0) ? 0 : Math.max.apply(null, array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    return (!array || array.length === 0) ? 0 : array.reduce((accumulator, currentValue) => (accumulator + currentValue)) / array.length;
 }
